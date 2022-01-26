107187a6b62e8b53ee11fce9df93be9c4f3bdff7
Ranjana Kambhammettu <ranjanak207@gmail.com>
2022-01-26 19:05:30 +0400

    Completed armstrong.rb

--- week1/armstrong.rb
+++ week1/armstrong.rb
@@ -9,5 +9,26 @@
 # and `false` otherwise.
 
 def armstrong_number(n)
-    raise NotImplementedError # Delete this line and start coding here
+    num = n
+    sum = 0
+    i = 0
+    arr = [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 16, 17, 19, 20, 21, 23, 24, 25, 27, 29, 31, 32, 33, 34, 35, 37, 38, 39] # From given reference link, these are the possible powers to generate an n-narcissistic number.
+    while (i<arr.length)
+        while (num>0)
+            d = num % 10
+            sum = sum + d**arr[i]
+            num = num / 10
+        end
+        if (n==sum)
+            return true
+        end
+        sum = 0
+        i = i + 1
+        num = n
+    end
+    if (n==sum)
+        return true
+    else
+        return false
+    end
 end
