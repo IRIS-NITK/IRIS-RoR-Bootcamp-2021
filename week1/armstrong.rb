5e5a2adcdd09e311fb9c1ab70a1e5749f7636e21
Adithya M S <rm224385@gmail.com>
2022-01-27 22:53:33 +0530

    Ruby assignment by AdithyaMS(201CS203)

--- week1/armstrong.rb
+++ week1/armstrong.rb
@@ -9,5 +9,19 @@
 # and `false` otherwise.
 
 def armstrong_number(n)
-    raise NotImplementedError # Delete this line and start coding here
+    tmp = n
+    sum = 0
+    n_digits = n.to_s.length
+
+    while n >= 1
+        rem = n%10
+        n = (n/10).floor()
+        sum += (rem**n_digits)
+    end
+    
+    if tmp == sum
+        return true
+    end
+
+    return false
 end
