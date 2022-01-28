219e536c5d8ab13ecbed38571992fc21bd0814f8
Vedant Tarale <vedanttarale10@gmail.com>
2022-01-28 18:13:30 +0530

    armstrong number

--- week1/armstrong.rb
+++ week1/armstrong.rb
@@ -10,5 +10,25 @@
 # and `false` otherwise.
 
 def armstrong_number(n)
-    raise NotImplementedError # Delete this line and start coding here
+    if n<0
+        puts false
+    else    
+        number = n
+        power = n.to_s.length
+        sum = 0
+        i = 0
+        while i!=power
+            sum += (number%10)**power
+            number/=10
+            i = i+1
+        end
+        if sum==n
+            puts true
+        else
+            puts false
+        end
+    end
 end
+print "Enter the number to be checked: "
+n = gets.chomp.to_i
+armstrong_number(n) 
