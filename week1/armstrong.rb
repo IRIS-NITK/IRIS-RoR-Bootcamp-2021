ae6984d263c4f5a7b58db5698dd08bd7fb26ba9c
shamithanaik <98634778+shamithanaik@users.noreply.github.com>
2022-01-29 20:17:50 +0530

    Update armstrong.rb

--- week1/armstrong.rb
+++ week1/armstrong.rb
@@ -9,5 +9,23 @@
 # and `false` otherwise.
 
 def armstrong_number(n)
-    raise NotImplementedError # Delete this line and start coding here
+   if n<0
+       return false
+   end
+    if n==0
+        return true
+    end
+    no_of_digits=math.log(n,10).floor+1
+    final=0
+    temp=n
+    while temp>0
+        rem=temp%10
+        final=final+ rem**no_of_digits
+        temp=temp/10
+    end
+    if (final==n)
+        return true
+    end
+    return false
+    
 end
