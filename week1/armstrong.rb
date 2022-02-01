16724968f76c8b2eb50ea9e89a73ccdad7323fd8
Nikhil Satish <satishnikhil307@gmail.com>
2022-02-01 18:51:24 +0530

    Assignment submission

--- week1/armstrong.rb
+++ week1/armstrong.rb
@@ -9,5 +9,28 @@
 # and `false` otherwise.
 
 def armstrong_number(n)
-    raise NotImplementedError # Delete this line and start coding here
+    # raise NotImplementedError # Delete this line and start coding here
+    if (n<0)
+      return false
+    end
+
+    num = n
+    spare = n
+    digits = 0
+    sum = 0
+    while (spare!=0)
+      spare = spare/10
+      digits = digits+1
+    end
+    while (n>0)
+      sum+=(n%10)**digits
+      n/=10
+    end
+    # return sum
+    if (sum==num)
+      return true
+    else
+      return false
+    end
 end
+# puts(armstrong_number(8208))
