c9d25ba3c16e66215a07511d6cb81f1a32b908e2
VedantTarale <vedanttarale10@gmail.com>
2022-02-01 19:13:44 +0530

    Assignment

--- week1/armstrong.rb
+++ week1/armstrong.rb
@@ -1,17 +1,6 @@
-# An armstrong number is a number such that sum of cubes of its digits
-# is equal to the number itself.
-#
-# For example,
-# 153 = 1^3 + 5^3 + 3^3
-#
-# `n` is a number provided to you as input.
-#
-# Your output should be `true` if the number is an Armstrong number
-# and `false` otherwise.
-
 def armstrong_number(n)
     if n<0
-        puts false
+        return false
     else    
         number_copy = n
         power = n.to_s.length
@@ -23,12 +12,9 @@ def armstrong_number(n)
             i = i+1
         end
         if sum==n
-            puts true
+            return true
         else
-            puts false
+            return false
         end
     end
 end
-print "Enter the number to be checked: "
-n = gets.chomp.to_i
-armstrong_number(n) 
