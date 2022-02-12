8012ce3b956635ed6a815968191c98a6fcb2dbaa
lohith <lohith1024@gmail.com>
2022-02-13 00:05:29 +0530

    mail and username workin

--- week3/about_me_app/app/controllers/about_controller.rb
+++ week3/about_me_app/app/controllers/about_controller.rb
@@ -2,9 +2,21 @@ class AboutController < ApplicationController
     
     def home
     end
-    # def formcontrol
-    #   word = params[:word]
-    #   render :update do |page|
+    def postpage
+      @name = params[:name]
+      @email=params[:email]
+      # render plain: "@name@email"
+    
+    #   render:update do |page|
     #   page.replace_html  'word_message', "#{word.length} characters"
-    # end
-end
+    end
+    def json
+      if params[:].key
+
+      end
+      respond_to do |format|
+        format.html{render plain:"hello "}
+        format.json{ render json:{message:"hello world"},status:404}
+      end
+    end
+end         
--- week3/about_me_app/config/routes.rb
+++ week3/about_me_app/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get "/about", to:"about#home"
-  root to:'about#home', as:"aboutpath"
-  
+  post "/" ,to:'about#postpage' , as:'about_page'
+  root to:'about#home', as:"rootpath"
+  get "/json",to:'about#json',as:'json_path'
+  post "/json",to:'about#json',as:'json_postpath'
 end
