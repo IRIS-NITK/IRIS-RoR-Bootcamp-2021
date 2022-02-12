0d6dd441d1a5750bcb41198b8e7aa8dd9e77564f
Prafful Gupta <praffulgupta09@gmail.com>
2022-02-12 18:36:20 +0530

    routes created

--- week3/about_me_app/config/routes.rb
+++ week3/about_me_app/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  # creating additional 3 routes
+  get '/about_page' , to: 'about_page#aboutPage' , as: 'aboutPage'
+  get '/about_form' , to: 'about_page#aboutForm' , as: 'aboutForm'
+  get '/about_json' , to: 'about_page#aboutJson' , as: 'aboutJson'
 
+
+  # changing root route
   root 'about_page#aboutPage'
 end
