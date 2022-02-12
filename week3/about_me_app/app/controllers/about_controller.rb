9ad1d42c2f655e36f3745620bf2e3ef4e256bf97
lohith <lohith1024@gmail.com>
2022-02-12 20:14:28 +0530

    latest commit

---
+++ week3/about_me_app/app/controllers/about_controller.rb
@@ -0,0 +1,10 @@
+class AboutController < ApplicationController
+    
+    def home
+    end
+    # def formcontrol
+    #   word = params[:word]
+    #   render :update do |page|
+    #   page.replace_html  'word_message', "#{word.length} characters"
+    # end
+end
