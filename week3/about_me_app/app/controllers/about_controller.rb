61455ecf561f6e03b5a59aea5031a0c2e89cc07d
lohith <lohith1024@gmail.com>
2022-02-13 11:08:47 +0530

    working json query final commit for w-3
    Signed-off-by: lohith <lohith1024@gmail.com>

--- week3/about_me_app/app/controllers/about_controller.rb
+++ week3/about_me_app/app/controllers/about_controller.rb
@@ -2,21 +2,23 @@ class AboutController < ApplicationController
     
     def home
     end
+    def about_form
+    end
     def postpage
       @name = params[:name]
       @email=params[:email]
-      # render plain: "@name@email"
-    
-    #   render:update do |page|
-    #   page.replace_html  'word_message', "#{word.length} characters"
     end
     def json
-      if params[:].key
-
-      end
+    @par=params[:message]
       respond_to do |format|
-        format.html{render plain:"hello "}
-        format.json{ render json:{message:"hello world"},status:404}
+         format.html{ redirect_to:rootpath }
+        if @par==nil
+         format.json{ render plain:" " ,status:400 }
+         else
+          format.json{render json:{ message:""+@par},status:200}
+        end         
       end
     end
+    def jsonpage
+    end  
 end         
--- week3/about_me_app/config/routes.rb
+++ week3/about_me_app/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get "/about", to:"about#home"
-  post "/" ,to:'about#postpage' , as:'about_page'
+  post "/about_form" ,to:'about#postpage'
+  get "/about_form" ,to:'about#about_form', as:'about_page'
   root to:'about#home', as:"rootpath"
-  get "/json",to:'about#json',as:'json_path'
-  post "/json",to:'about#json',as:'json_postpath'
+  get "/about_json",to:'about#json',as:'json_path'
+  get "/json",to:'about#jsonpage'
 end
