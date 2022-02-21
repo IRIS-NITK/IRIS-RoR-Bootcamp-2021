c68a07c955a92a5f75adcfbf7f32977ed06d4dda
VedantTarale <vedanttarale10@gmail.com>
2022-02-21 22:16:45 +0530

    Week 3 Updated

--- week3/about_me_app/app/controllers/about_page_controller.rb
+++ week3/about_me_app/app/controllers/about_page_controller.rb
@@ -2,8 +2,13 @@ class AboutPageController < ApplicationController
     def home
     end
     def page
-        @name = params[:name]
-        @email = params[:email]
+        if params[:name].present? and params[:email].present?
+            @name = params[:name]
+            @email = params[:email]
+        else
+            redirect_to root_path    
+        end
+        
     end
     def form
     end
@@ -13,9 +18,9 @@ class AboutPageController < ApplicationController
             format.any {render plain: "Invalid Request Format", status: :bad_request}
             format.json {
                 if params[:message]!=nil
-                    render json: {"message": params[:message]}, status: :ok
+                    render json: {message: params[:message]}, status: :ok
                 else
-                    render json: {"message": "INVALID",status: :bad_request}
+                    render json: {message: "INVALID"}, status: :bad_request
                     # return status: :bad_request
                 end
             }
