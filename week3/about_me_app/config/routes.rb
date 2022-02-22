8e6573ea93760b995370b7bb63d415503a785a56
Prafful Gupta <praffulgupta09@gmail.com>
2022-02-22 13:06:46 +0530

    assign

--- week3/about_me_app/config/routes.rb
+++ week3/about_me_app/config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
   # creating additional 3 routes
-  get '/about_page' , to: 'about_page#aboutPage' , as: 'aboutPage'
-  get '/about_form' , to: 'about_page#aboutForm' , as: 'aboutForm'
-  get '/about_json' , to: 'about_page#aboutJson' , as: 'aboutJson'
+  post 'about_page' , to: 'about_page#about_page'
+  get 'about_form' , to: 'about_page#about_form'
+  get 'about_json' , to: 'about_page#about_json'
 
 
   # changing root route
-  root 'about_page#aboutPage'
+  root 'about_page#index'
 end
