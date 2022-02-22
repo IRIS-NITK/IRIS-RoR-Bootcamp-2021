30b166989cc028d39584ec803c0a0ff133236df6
Prafful Gupta <praffulgupta09@gmail.com>
2022-02-22 10:48:11 +0530

    about page controller

--- week3/about_me_app/app/controllers/about_page_controller.rb
+++ week3/about_me_app/app/controllers/about_page_controller.rb
@@ -1,14 +1,2 @@
 class AboutPageController < ApplicationController
-
-    def aboutPage
-        render plain: "Prafful Gupta"
-    end
-
-    def aboutForm
-        render plain: "Form Page"
-    end
-
-    def aboutJson
-        render plain: "Json Page"
-    end
 end
