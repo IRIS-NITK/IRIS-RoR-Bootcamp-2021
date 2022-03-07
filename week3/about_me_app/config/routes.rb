6812a4ff0b4cea92ab44e47aedb93b7bce46ccab
diviz11 <d.akkshat@gmail.com>
2022-03-07 17:09:30 +0530

    week3 assignment

--- week3/about_me_app/config/routes.rb
+++ week3/about_me_app/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'about_page#index'
+  
+  post '/about_page', to: 'about_page#about_page'
+
+  get '/about_form', to: 'about_page#about_form'
+
+  get '/about_json', to: 'about_page#about_json'
+
 
 end
