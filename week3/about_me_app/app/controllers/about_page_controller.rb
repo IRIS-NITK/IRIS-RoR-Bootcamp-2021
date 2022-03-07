35c1b3681fedd037a9b01df7df08ba809b92c7c8
Anagha HC <hcanagha@gmail.com>
2022-03-07 13:21:27 +0530

    assignment 3 modified

--- week3/about_me_app/app/controllers/about_page_controller.rb
+++ week3/about_me_app/app/controllers/about_page_controller.rb
@@ -1,29 +1,37 @@
 class AboutPageController < ApplicationController
     
- 
-  def form
-  end
-
-  def about_form
-  end
-      
-  end
-
-  def about_page
-    @name = params[:name]
-    @email = params[:email]
-  end
-
-  def about_json
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.json {
-        unless params[:message] == nil
-        render json: { message: params[:message] }, status: :ok
-        else
-          render json: { message: "Invalid" }, status: :bad_request
-        end
-      }
-      format.any { render plain: "Invalid Request Format", status: :bad_request}
+    before_action :check_message, only: :about_json
+    
+    def pagedef
     end
-  end
+
+    def about_form
+    end
+
+    def about_page
+       if params[:email].present? and params[:name].present?
+           @name =params[:name]
+           @email =params[:email]
+        else
+          redirect_to root_path
+        end
+    end
+    
+    def about_json
+        respond_to do |format|
+            format.html { redirect_to root_path}
+            format.json { render json: {message: params[:message]}, status: :ok}
+            format.any { render plain: "Invalid Request Format", status: :bad_request}
+        end
+    end
+        
+
+    private 
+
+    def check_message
+        unless params[:message].present?
+           # render plain: "bad request", status :bad_request
+           head :bad_request
+        end
+    end
+end
