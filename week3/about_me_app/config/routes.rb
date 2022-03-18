c6bb564dc46cdc2bf98019337fe372816a621f7e
Anagha HC <hcanagha@gmail.com>
2022-03-18 11:19:23 +0530

    Change in routes

--- week3/about_me_app/config/routes.rb
+++ week3/about_me_app/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root 'about_page#about_form'
-post 'about_page/about_page'
-get 'about_page/about_json'
-get 'about_page/about_form'
+ root 'about_page#index'
+  
+  post '/about_page', to: 'about_page#about_page'
 
+  get '/about_form', to: 'about_page#about_form'
+
+  get '/about_json', to: 'about_page#about_json'
 end
